0f342c2dbd784e72d92a86aa6d9ce5a18cc351cc
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-07-21 18:02:50 +0530

    Started Implementing CharacterStats and LevelManager. and created a function AddExp() for testing

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int[] expProgression;
+    public int[][] statsProgression;
+
+    private int currentExp = 0;
+    private int currentLevel = 0;
+    private int maxLevel = 9;
+
+    public LevelManager()
+    {
+
+    }
+
+
+    public void AddExp(int expToAdd, out int currentExp, out int currentLevel)
+    {
+        if(this.currentLevel == maxLevel)
+        {
+            currentExp = 0;
+            currentLevel = 9;
+            return;
+        }
+
+        currentExp = this.currentExp;
+        currentLevel = this.currentLevel;
+
+        currentExp += expToAdd;
+
+        if(currentExp >= expProgression[currentLevel])
+        {
+            currentExp -= expProgression[currentLevel];
+            currentLevel++;
+        }
+
+        this.currentExp = currentExp;
+        this.currentLevel = currentLevel;
+        
+    }
+
+
+}
